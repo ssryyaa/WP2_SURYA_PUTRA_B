751c61f502f06b6a40ffb2cbf677ffc50c1524a0
ssryyaa <145754874+ssryyaa@users.noreply.github.com>
2023-10-27 14:22:06 +0700

    Update contoh1.php

--- contoh1.php
+++ contoh1.php
@@ -4,6 +4,6 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo"Nama saya Surya Putra saya tinggal di Solo dan Olahraga yang saya sukai adalah Bulutangkis";
+        echo"Nama saya Surya Putra saya tinggal di Solo dan Olahraga yang saya sukai adalah Bersepeda";
     }
 }
